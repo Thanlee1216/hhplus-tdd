cfb2406369e5a8f819a07394cd48592dea497287
leetaehan <lth9312@logossystem.co.kr>
2024-06-18 14:06:39 +0900

    E2E 테스트를 위한 사전 준비

--- src/main/java/io/hhplus/tdd/exception/ApiControllerAdvice.java
+++ src/main/java/io/hhplus/tdd/exception/ApiControllerAdvice.java
@@ -1,5 +1,6 @@
-package io.hhplus.tdd;
+package io.hhplus.tdd.exception;
 
+import io.hhplus.tdd.ErrorResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -11,4 +12,9 @@ class ApiControllerAdvice extends ResponseEntityExceptionHandler {
     public ResponseEntity<ErrorResponse> handleException(Exception e) {
         return ResponseEntity.status(500).body(new ErrorResponse("500", "에러가 발생했습니다."));
     }
+    @ExceptionHandler(value = PointException.class)
+    public ResponseEntity<ErrorResponse> handlePointException(PointException e) {
+        return ResponseEntity.status(500).body(new ErrorResponse("500", ExceptionType.getMessage(e.getCode())));
+    }
 }
+
---
+++ src/main/java/io/hhplus/tdd/point/service/PointServiceImpl.java
@@ -0,0 +1,13 @@
+package io.hhplus.tdd.point.service;
+
+import io.hhplus.tdd.point.UserPoint;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PointServiceImpl implements PointService {
+
+    @Override
+    public UserPoint getUserPoint(Long id) {
+        return null;
+    }
+}
