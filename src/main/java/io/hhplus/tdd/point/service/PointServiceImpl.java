c1f837cac4e840e3baa68908e9ebc45de740d216
leetaehan <lth9312@logossystem.co.kr>
2024-06-19 17:13:31 +0900

    Service 단위 테스트 2차 커밋

--- src/main/java/io/hhplus/tdd/point/service/PointServiceImpl.java
+++ src/main/java/io/hhplus/tdd/point/service/PointServiceImpl.java
@@ -1,6 +1,7 @@
 package io.hhplus.tdd.point.service;
 
 import io.hhplus.tdd.point.PointHistory;
+import io.hhplus.tdd.point.TransactionType;
 import io.hhplus.tdd.point.UserPoint;
 import io.hhplus.tdd.point.repository.PointRepository;
 import org.springframework.stereotype.Service;
@@ -18,7 +19,14 @@ public class PointServiceImpl implements PointService {
 
     @Override
     public UserPoint getUserPoint(Long id) {
-        return repository.getUserPoint(id);
+        UserPoint userPoint = repository.getUserPoint(id);
+        long time = System.currentTimeMillis() - userPoint.updateMillis();
+        if(time < 500) { //0.5초 안에 수정되었다면 신규 계정으로 판단..? TODO - 의문이 드는 방법이기 때문에 대안을 생각해야함
+            userPoint = repository.insertUserPoint(userPoint.id(), userPoint.point());
+            //신규 고객은 0포인트 충전 이력을 등록 TODO - 충전에 실패하면 롤백해야하나 UserPointTable에는 현재 삭제 메소드가 없기에 생략
+            repository.insertPointHistory(userPoint.id(), userPoint.point(), TransactionType.CHARGE, System.currentTimeMillis());
+        }
+        return userPoint;
     }
 
     @Override
--- src/test/java/io/hhplus/tdd/point/service/PointServiceTest.java
+++ src/test/java/io/hhplus/tdd/point/service/PointServiceTest.java
@@ -1,13 +1,19 @@
 package io.hhplus.tdd.point.service;
 
+import io.hhplus.tdd.database.UserPointTable;
 import io.hhplus.tdd.point.UserPoint;
 import io.hhplus.tdd.point.repository.PointRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
@@ -21,27 +27,39 @@ class PointServiceTest {
     @Mock
     PointRepository pointRepository;
 
+    UserPointTable userPointTable;
+
+    @BeforeEach
+    void setUp() {
+        userPointTable = new UserPointTable();
+    }
+
     /**
      * id에 해당하는 유저를 조회해오기 위한 테스트
      */
     @Test
-    void 포인트_조회() {
+    void 유저를_조회해온다() {
         long id = 1L;
         when(pointRepository.getUserPoint(id)).thenReturn(new UserPoint(id, 0, 0));
         UserPoint userPoint = pointService.getUserPoint(id);
-        assertEquals(id, userPoint.id());
+        assertThat(userPoint.id()).isEqualTo(id);
     }
 
     /**
      * 존재하지 않는 유저를 조회하는 테스트
-     * service에서 NFE를 발생시키고 컨트롤러에서 CustomException을 발생
-     * TODO - 존재하지 않는 유저를 조회하면 에러를 발생시킬 것인가, 기존 메소드대로 신규 생성을 할 것인가 고민 필요
+     * 존재하지 않는 유저는 신규로 등록하고 0포인트를 충전한 이력을 남긴다.
      */
     @Test
-    void 포인트_조회_실패() {
+    void 존재하지_않는_유저는_신규로_등록하고_0포인트_충전_이력을_생성한다() {
         long id = 1L;
-        when(pointRepository.getUserPoint(id)).thenThrow(NullPointerException.class);
-        assertThrows(NullPointerException.class, () -> pointService.getUserPoint(id));
+        UserPoint userPoint = UserPoint.empty(id);
+        when(pointRepository.getUserPoint(id)).thenReturn(userPoint); //생성되는 신규 유저의 생성 시간을 0으로 세팅
+        when(pointRepository.insertUserPoint(userPoint.id(), userPoint.point())).thenReturn(userPointTable.insertOrUpdate(userPoint.id(), userPoint.point())); //신규 유저 등록의 결과는 유저 포인트 테이블의 등록 결과와 같음
+
+        UserPoint resultUserPoint = pointService.getUserPoint(id);
+
+        assertThat(resultUserPoint.id()).isEqualTo(id);
+        assertThat(resultUserPoint.updateMillis()).isNotEqualTo(userPoint.updateMillis());//신규 생성된 user는 UserPoint.empty(id)의 결과인 System.currentTimeMillis()값을 가진다
     }
   
 }
