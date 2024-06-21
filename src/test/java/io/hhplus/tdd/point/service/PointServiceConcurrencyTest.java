36d0bfc4d37d64e31ba50177e43738e16806ff21
LeeTaeHan <lth9312@gmail.com>
2024-06-21 18:21:41 +0900

    동시성 테스트 2차 커밋 //Synchronization -> ReentrantLock

--- src/test/java/io/hhplus/tdd/point/service/PointServiceConcurrencyTest.java
+++ src/test/java/io/hhplus/tdd/point/service/PointServiceConcurrencyTest.java
@@ -16,7 +16,7 @@ public class PointServiceConcurrencyTest {
     PointService pointService;
 
     @Test
-    void 동시성_테스트() {
+    void 충전과_사용이_동시에_일어나는_테스트() {
         //given
         long id = 1L;
         UserPoint userPoint = pointService.getUserPoint(id);
@@ -35,8 +35,32 @@ public class PointServiceConcurrencyTest {
                 })
         ).join();
 
-        //then
         UserPoint resultUserPoint = pointService.getUserPoint(id);
         assertThat(resultUserPoint.point()).isEqualTo(100000L + 10000L - 20000L - 300L);
+    }
+
+    @Test
+    void 먼저_요청된_충전이_처리되어야_사용이_가능한_경우의_테스트() {
+        //given
+        long id = 1L;
+        UserPoint userPoint = pointService.getUserPoint(id);
+        pointService.chargePoint(userPoint.id(), 10300L);
+
+        //when
+        CompletableFuture.allOf(
+                CompletableFuture.runAsync(() -> {
+                    pointService.chargePoint(userPoint.id(), 10000L);
+                }),
+                CompletableFuture.runAsync(() -> {
+                    pointService.usePoint(userPoint.id(), 20000L);
+                }),
+                CompletableFuture.runAsync(() -> {
+                    pointService.usePoint(userPoint.id(), 300L);
+                })
+        ).join();
+
+        //then
+        UserPoint resultUserPoint = pointService.getUserPoint(id);
+        assertThat(resultUserPoint.point()).isEqualTo(10300L + 10000L - 20000L - 300L);
     }
 }
